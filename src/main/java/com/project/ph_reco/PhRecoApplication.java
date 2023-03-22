599b8480ae508b96a3b69f1c91f52d947be34d9f
yang <gluestuck@naver.com>
2023-03-22 21:43:49 +0900

    first commit

---
+++ src/main/java/com/project/ph_reco/PhRecoApplication.java
@@ -0,0 +1,13 @@
+package com.project.ph_reco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhRecoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhRecoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/project/ph_reco/PhRecoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.ph_reco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhRecoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
