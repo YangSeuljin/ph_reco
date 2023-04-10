34a7f72a241ea3be39492555be5796e794e5f090
yang <gluestuck@naver.com>
2023-04-10 22:53:03 +0900

    - JPA Dirty Checking 테스트

---
+++ src/main/java/com/project/ph_reco/pharmacy/service/PharmacyRepositoryService.java
@@ -0,0 +1,41 @@
+package com.project.ph_reco.pharmacy.service;
+
+import com.project.ph_reco.pharmacy.entity.Pharmacy;
+import com.project.ph_reco.pharmacy.repository.PharmacyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+import java.util.Objects;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class PharmacyRepositoryService {
+    private final PharmacyRepository pharmacyRepository;
+
+    @Transactional
+    public void updateAddress(Long id, String address) {
+        Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
+
+        if (Objects.isNull(entity)) {
+            log.error("not found id: {}", id);
+            return;
+        }
+
+        entity.changePharmacyAddress(address);
+    }
+
+    //for test
+    public void updateAddressWithoutTransaction(Long id, String address) {
+        Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
+
+        if (Objects.isNull(entity)) {
+            log.error("not found id: {}", id);
+            return;
+        }
+
+        entity.changePharmacyAddress(address);
+    }
+}
