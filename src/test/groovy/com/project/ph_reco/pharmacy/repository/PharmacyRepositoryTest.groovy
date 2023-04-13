d37e13d3e13c59f5dae253362073c117e4d9513e
yang <gluestuck@naver.com>
2023-04-13 09:12:01 +0900

    - 약국 저장 테스트 코드 작성

--- src/test/groovy/com/project/ph_reco/pharmacy/repository/PharmacyRepositoryTest.groovy
+++ src/test/groovy/com/project/ph_reco/pharmacy/repository/PharmacyRepositoryTest.groovy
@@ -4,11 +4,17 @@ import com.project.ph_reco.AbstractIntegrationContainerBaseTest
 import com.project.ph_reco.pharmacy.entity.Pharmacy
 import org.springframework.beans.factory.annotation.Autowired
 
+import java.time.LocalDateTime
+
 
 class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
 
     @Autowired
     private PharmacyRepository pharmacyRepository;
+
+    def setup() {
+        pharmacyRepository.deleteAll();
+    }
 
     def "PharmacyRepository save"() {
         given:
@@ -18,7 +24,7 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         double longitude = 128.11
 
         def pharmacy = Pharmacy.builder()
-                .pharmacyAdress(address)
+                .pharmacyAddress(address)
                 .pharmacyName(name)
                 .latitude(latitude)
                 .longitude(longitude)
@@ -27,9 +33,53 @@ class PharmacyRepositoryTest extends AbstractIntegrationContainerBaseTest {
         when:
         def result = pharmacyRepository.save(pharmacy);
         then:
-        result.getPharmacyAdress() == address
+        result.getPharmacyAddress() == address
         result.getPharmacyName() == name
         result.getLatitude() == latitude
         result.getLongitude() == longitude
+    }
+
+    def "PharmacyRepository SaveAll"() {
+        given:
+        String address = "서울특별시 성북구 종암동"
+        String name = "은혜 약국"
+        double latitude = 36.11
+        double longitude = 128.11
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .latitude(latitude)
+                .longitude(longitude)
+                .build()
+
+        when:
+        pharmacyRepository.saveAll(Arrays.asList(pharmacy))
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.size() == 1
+
+    }
+
+    def "BaseTimeEntity 등록"() {
+        given:
+        LocalDateTime now = LocalDateTime.now()
+        String address = "서울특별시 성북구 종암동"
+        String name = "은혜약국"
+        println "now = $now"
+
+        def pharmacy = Pharmacy.builder()
+                .pharmacyAddress(address)
+                .pharmacyName(name)
+                .build()
+        when:
+        pharmacyRepository.save(pharmacy)
+        def result = pharmacyRepository.findAll()
+        println "result = $result"
+        then:
+        result.get(0).getCreatedDate().isAfter(now)
+        result.get(0).getModifiedDate().isAfter(now)
+
     }
 }
