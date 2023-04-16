51a43cfeb91ff5f43df682bec868e6cb0faa913d
yang <gluestuck@naver.com>
2023-04-16 15:06:26 +0900

    - 약국전체 정보 가져오는 메서드 추가

--- src/main/java/com/project/ph_reco/pharmacy/service/PharmacyRepositoryService.java
+++ src/main/java/com/project/ph_reco/pharmacy/service/PharmacyRepositoryService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -37,5 +38,10 @@ public class PharmacyRepositoryService {
         }
 
         entity.changePharmacyAddress(address);
+    }
+
+    @Transactional
+    public List<Pharmacy> findAll() {
+        return pharmacyRepository.findAll();
     }
 }
