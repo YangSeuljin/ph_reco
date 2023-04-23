57a5e1c8d06c89a5981c30012e76eb38d4dc763e
yang <gluestuck@naver.com>
2023-04-23 23:00:28 +0900

    - 주소 변경 정보 테이블 Repository 생성

---
+++ src/main/java/com/project/ph_reco/direction/Repository/DirectionRepository.java
@@ -0,0 +1,7 @@
+package com.project.ph_reco.direction.Repository;
+
+import com.project.ph_reco.direction.entity.Direction;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DirectionRepository extends JpaRepository<Direction, Long> {
+}
