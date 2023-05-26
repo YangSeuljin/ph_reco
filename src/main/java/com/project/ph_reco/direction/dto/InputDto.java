c5c33fcf1634452b78f21794d2c5ed4b75dd59ac
yang <gluestuck@naver.com>
2023-05-26 23:18:28 +0900

    - 입력 정보(주소)를 입력받는 dto 생성

---
+++ src/main/java/com/project/ph_reco/direction/dto/InputDto.java
@@ -0,0 +1,10 @@
+package com.project.ph_reco.direction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class InputDto {
+    private String address;
+}
