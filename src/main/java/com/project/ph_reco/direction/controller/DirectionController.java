29cbecd85c5404e6d3511509f48efc0d484cd768
yang <gluestuck@naver.com>
2023-06-13 22:29:12 +0900

    - url encode/decode 로직을 Controller -> Service로 이동

--- src/main/java/com/project/ph_reco/direction/controller/DirectionController.java
+++ src/main/java/com/project/ph_reco/direction/controller/DirectionController.java
@@ -1,13 +1,11 @@
 package com.project.ph_reco.direction.controller;
 
-import com.project.ph_reco.direction.entity.Direction;
 import com.project.ph_reco.direction.service.DirectionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 @Slf4j
@@ -17,17 +15,12 @@ public class DirectionController {
     private final DirectionService directionService;
     private static final String DIRECTION_BASE_URL = "https://map.kakao.com/link/map/";
 
+
     @GetMapping("/dir/{encodeId}")
     public String searchDirection(@PathVariable("encodeId") String encodeId) {
-        Direction resultDirection = directionService.findById(encodeId);
+        String result = directionService.findDirectionUrlById(encodeId);
 
-        String params = String.join(",", resultDirection.getTargetPharmacyName(),
-                String.valueOf(resultDirection.getTargetLatitude()), String.valueOf(resultDirection.getTargetLongitude()));
-
-        //은혜약국, 38.11, 128.11
-        String result = UriComponentsBuilder.fromHttpUrl(DIRECTION_BASE_URL + params).toUriString();
-
-        log.info("direction params: {}, url: {}", params, result);
+        log.info("[DirectionController searchDirection] direction url : {}", result);
 
         return "redirect:" + result;
     }
