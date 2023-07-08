27b33cc54ad93d6eb42a526e5fe48e2bc54a5867
yang <gluestuck@naver.com>
2023-07-08 13:45:21 +0900

    - redis에 데이터가 없을 경우 데이터베이스를 조회하도록 수정

--- src/main/java/com/project/ph_reco/pharmacy/service/PharmacySearchService.java
+++ src/main/java/com/project/ph_reco/pharmacy/service/PharmacySearchService.java
@@ -5,6 +5,7 @@ import com.project.ph_reco.pharmacy.dto.PharmacyDto;
 import com.project.ph_reco.pharmacy.entity.Pharmacy;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -21,11 +22,11 @@ public class PharmacySearchService {
 
     public List<PharmacyDto> searchPharmacyDtoList() {
 
-        //redis
+        // redis
         List<PharmacyDto> pharmacyDtoList = pharmacyRedisTemplateService.findAll();
-        if (pharmacyDtoList.isEmpty()) return pharmacyDtoList;
+        if(CollectionUtils.isNotEmpty(pharmacyDtoList)) return pharmacyDtoList;
 
-        //db
+        // db
         return pharmacyRepositoryService.findAll()
                 .stream()
                 .map(this::convertToPharmacyDto)
