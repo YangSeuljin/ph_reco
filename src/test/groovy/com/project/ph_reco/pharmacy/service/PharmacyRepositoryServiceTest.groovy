ac071b0083a67cb696cbc439d0a7e5f224077073
yang <gluestuck@naver.com>
2023-07-08 13:46:30 +0900

    - pharmacyRepository 테스트 코드 작성

---
+++ src/test/groovy/com/project/ph_reco/pharmacy/service/PharmacyRepositoryServiceTest.groovy
@@ -0,0 +1,56 @@
+package com.project.ph_reco.pharmacy.service
+
+import com.project.ph_reco.AbstractIntegrationContainerBaseTest
+import com.project.ph_reco.pharmacy.entity.Pharmacy
+import com.project.ph_reco.pharmacy.repository.PharmacyRepository
+import org.springframework.beans.factory.annotation.Autowired
+
+class PharmacyRepositoryServiceTest extends AbstractIntegrationContainerBaseTest {
+
+    @Autowired
+    PharmacyRepositoryService pharmacyRepositoryService;
+
+    @Autowired
+    private PharmacyRepository pharmacyRepository;
+
+    def setup() {
+        pharmacyRepository.deleteAll();
+    }
+
+    def "PharmacyRepository update - dirty checking success"() {
+        given:
+        String inputAddress = "서울특별시 성북구 종암동"
+        String modifiedAddress = "서울 광진구 구의동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder().pharmacyAddress(inputAddress).pharmacyName(name).build()
+
+        when:
+        def entity = pharmacyRepository.save(pharmacy)
+        pharmacyRepositoryService.updateAddress(entity.getId(), modifiedAddress)
+
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getPharmacyAddress() == modifiedAddress
+    }
+
+    def "PharmacyRepository update - dirty checking fail"() {
+        given:
+        String inputAddress = "서울특별시 성북구 종암동"
+        String modifiedAddress = "서울 광진구 구의동"
+        String name = "은혜 약국"
+
+        def pharmacy = Pharmacy.builder().pharmacyAddress(inputAddress).pharmacyName(name).build()
+
+        when:
+        def entity = pharmacyRepository.save(pharmacy)
+        pharmacyRepositoryService.updateAddressWithoutTransaction(entity.getId(), modifiedAddress)
+
+        def result = pharmacyRepository.findAll()
+
+        then:
+        result.get(0).getPharmacyAddress() == inputAddress
+    }
+
+}
